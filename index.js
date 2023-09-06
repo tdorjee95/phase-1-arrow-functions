d12064ed0c5ae963851cfac400bdde11435b9c59
Tenzin Dorjee <tenzindorjee95@yahoo.com>
2023-09-06 01:14:13 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide (num1 , num2) {
+  return num1 / num2;
+}
+
+divide(2000,100);
+
+const square = (parameter1) => parameter1 ** 2;
+square(2);
+
+const add = (parameter1 , parameter2) => parameter1 + parameter2;
